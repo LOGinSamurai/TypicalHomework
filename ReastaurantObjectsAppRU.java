07b22ca2a11fd0fbeab670503ee6985d29ad7ede
LOGinSamurai <52347454+LOGinSamurai@users.noreply.github.com>
2019-07-07 12:19:28 +0300

    Add files via upload

---
+++ ReastaurantObjectsAppRU.java
@@ -0,0 +1,118 @@
+
+public class ReastaurantObjectsAppRU {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+//предположим есть 3 класса
+//что-то что можно употребл€ть как пища
+//1) добавить constructor,setters,getters,tostring
+class Aliment{
+ private String name;
+ private String price;
+public Aliment(String name, String price) {
+	this.name = name;
+	this.price = price;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getPrice() {
+	return price;
+}
+public void setPrice(String price) {
+	this.price = price;
+}
+@Override
+public String toString() {
+	return "Aliment [name=" + name + ", price=" + price + "]";
+}
+
+
+ 
+}
+class Food extends Aliment{
+ private double weight;    
+ // kg
+
+public Food(String name, String price, double weight) {
+	super(name, price);
+	this.weight = weight;
+}
+
+public double getWeight() {
+	return weight;
+}
+
+public void setWeight(double weight) {
+	this.weight = weight;
+}
+
+@Override
+public String toString() {
+	return "Food [weight=" + weight + "]";
+}
+
+
+
+	
+}
+class Drink extends Aliment{
+ private double volume;  
+ // L
+
+public Drink(String name, String price, double volume) {
+	super(name, price);
+	this.volume = volume;
+}
+
+public double getVolume() {
+	return volume;
+}
+
+public void setVolume(double volume) {
+	this.volume = volume;
+}
+
+@Override
+public String toString() {
+	return "Drink [volume=" + volume + "]";
+}
+
+}
+
+
+//есть так же класс места где это все подают
+//допишите логику так чтобы
+//2) в массив с пищей во€вилось 6 обьектов (3 - напитки / 3 - еда  )
+//3) printMenu() вывел на экран перечень всех пунтков
+//4) printMenu(int,int) вывел на экран перечень всех пунтков меню с этим диапазоном стоимости!
+class Restaurant{
+ private String name;
+ private Aliment[] menu;  // список блюд и напитков (ѕќЋ»ћќ–‘»«ћ!)
+ public Restaurant(String name){
+     this.name = name;
+     this.menu = new Aliment[6]; // резервируем 6 пунтков меню!
+     // первые 3 - напитки
+     for(int i = 0; i < 3; i++){
+         this.menu[i] = new Drink(Drink.classgetName(), getPrice(), GetVolume());
+     }
+     // след 3 - еда
+     for(int i = 0; i < 3; i++){
+         this.menu[i] = new Food(????);
+     }
+ }
+ public void printMenu(){
+     ???
+ }
+ public void printMenu(int min_price,int max_price){
+     ???
+ }
+ 
+}
