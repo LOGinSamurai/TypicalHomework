b2f91e97472fc0af6fd2ee41ef438a4622b40bda
LOGinSamurai <52347454+LOGinSamurai@users.noreply.github.com>
2019-07-16 20:36:26 +0300

    Update ObjectRelationshipApp.java

--- ObjectRelationshipApp.java
+++ ObjectRelationshipApp.java
@@ -2,8 +2,62 @@
 public class ObjectRelationshipApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Woman w = new Woman("Anastasia");
+		Man m = new Man("Ivan");
+		House h = new House("Street",200);
+		
+        //relations - house
+		m.propriety = h;
+		h.owner = m;
+		
+		//relations -mariage
+		w.husband = m;
+		m.wife    = w;
+		m.wife.hello();
+	 
+		
+	    //
 
 	}
 
 }
+ class Woman{
+	 public House propriety;
+	 public String fullname;
+public Man husband;
+	public Woman(String fullname) {
+		this.fullname = fullname;
+	}
+	public void hello(){
+		System.out.println("oooo");
+	}
+	 
+	 
+ }
+ 
+ class Man{
+	 public House propriety;
+	 public String fullname;
+public Woman wife;
+	public Man(String fullname) {
+		this.fullname = fullname;
+	}
+	 
+	 
+ }
+ class House{
+	 Object owner;
+	 public String address;
+	 public float area;
+	public House(String address, float area) {
+		
+		this.address = address;
+		this.area = area;
+	}
+	 
+ }
+ 
+ 
+ 
+ 
+ 
